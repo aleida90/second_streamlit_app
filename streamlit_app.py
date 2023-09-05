5df711ca6f9122d36789f29c56b02bff1f761ee7
aleida90 <42223986+aleida90@users.noreply.github.com>
2023-09-04 19:05:57 -0600

    Update streamlit_app.py
    agregando iconos

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import streamlit
 streamlit.title('My Parents new healthy dinner!')
 
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Bluebery Oatmeal')
-streamlit.text('Kale, spinach, & Rocket smoothie')
-streamlit.text('Hard-boiled free-range egg')
+streamlit.text('🥣Omega 3 & Bluebery Oatmeal')
+streamlit.text('🥗Kale, spinach, & Rocket smoothie')
+streamlit.text('🐔Hard-boiled free-range egg')
+streamlit.text('🥑🍞Avocado toast')
