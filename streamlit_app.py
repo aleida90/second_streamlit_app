f6e4bbba5c5d53b6697006140f820f4c687bbaa8
aleida90 <42223986+aleida90@users.noreply.github.com>
2023-09-04 19:03:35 -0600

    Update streamlit_app.py
    agregando el menu, con funcion de texto

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('My Parents new healthy dinner!')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Bluebery Oatmeal')
+streamlit.text('Kale, spinach, & Rocket smoothie')
+streamlit.text('Hard-boiled free-range egg')
