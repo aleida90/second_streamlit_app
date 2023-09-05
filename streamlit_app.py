6ea320fc6c50fe595004f4b861dbf423a7d5c767
aleida90 <42223986+aleida90@users.noreply.github.com>
2023-09-05 13:52:31 -0600

    Update streamlit_app.py
    trying again

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,5 +13,6 @@ streamlit.header('🏋️‍♀️Fitness Menu (an extra portion of protein!)')
 streamlit.text('🍗🥗chicken with salad')
 streamlit.text('🥩🍚 meat and rice')
 
+streamlit.header('🍇🍈🍉Build your own smoothie🍒🥝🍎🍍')
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)
